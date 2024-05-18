4d93e0d9bf2651922ab14b5d55cc9897e8f8ec78
Roc <2023_roc.bigas@iticbcn.cat>
2024-05-18 12:44:33 +0200

    Intento de añadir el modulo en el menu para ver su uso , con el MENUITEM, aun no funciona

--- odoo/odoo-custom-addons/testmodulo/models/__init__.py
+++ odoo/odoo-custom-addons/testmodulo/models/__init__.py
@@ -1,5 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from . import event
-from . import cleaning_schedule
-from . import touristic_outing
+from . import models
--- odoo/odoo-custom-addons/testmodulo/models/models.py
+++ odoo/odoo-custom-addons/testmodulo/models/models.py
@@ -1,18 +1,46 @@
-# -*- coding: utf-8 -*-
+from odoo import models, fields
 
-# from odoo import models, fields, api
+class cleaning_schedule(models.Model):
+    _name = 'testmodulo.cleaning_schedule'
+    _description = 'Horario de limpieza'
 
+    name = fields.Char(string='Nombre de limpieza', required=True)
+    date = fields.Date(string='Fecha', required=True)
+    description = fields.Text(string='Descripción')
 
-# class testmodulo(models.Model):
-#     _name = 'testmodulo.testmodulo'
-#     _description = 'testmodulo.testmodulo'
+class event(models.Model):
+    _name = 'testmodulo.event'
+    _description = 'Eventos'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    name = fields.Char(string='Nombre del evento', required=True)
+    start_datetime = fields.Datetime(string='Inicio del evento', required=True)
+    end_datetime = fields.Datetime(string='Fin del evento', required=True)
+    description = fields.Text(string='Descripción')
+    
+    # Servicios con hora de inicio y fin
+    spa_service_start = fields.Datetime(string='Inicio de spa')
+    spa_service_end = fields.Datetime(string='Cierre de spa')
+    
+    pool_service_start = fields.Datetime(string='Inicio de piscina')
+    pool_service_end = fields.Datetime(string='Cierre de piscina')
+    
+    breakfast_start = fields.Datetime(string='Inicio de desayuno')
+    breakfast_end = fields.Datetime(string='Cierre de desayuno')
+    
+    lunch_start = fields.Datetime(string='Inicio de comida')
+    lunch_end = fields.Datetime(string='Cierre de comida')
+    
+    dinner_start = fields.Datetime(string='Inicio de cena')
+    dinner_end = fields.Datetime(string='Cierre de cena')
+    
+    night_dj_start = fields.Datetime(string='Inicio de DJ')
+    night_dj_end = fields.Datetime(string='Cierre de DJ')
+
+class touristic_outing(models.Model):
+    _name = 'testmodulo.touristic_outing'
+    _description = 'Salidas turísticas'
+
+    name = fields.Char(string='Nombre de la ruta', required=True)
+    start_date = fields.Date(string='Fecha de inicio', required=True)
+    end_date = fields.Date(string='Fecha de finalización', required=True)
+    description = fields.Text(string='Descripción')
